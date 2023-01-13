87d1b36da95f3a9a33c90af4b7726222c25ebf2f
joetsu-goto <gotou0600@gmail.com>
2023-01-13 11:52:24 +0900

    404ページ変更

--- 404.php
+++ 404.php
@@ -1,38 +1,19 @@
 <?php
+
 /**
  * 404
  */
 
 get_header();
-get_template_part( 'parts/template/mainvisual/page' );
-get_template_part( 'parts/breadcrumb' );
+get_template_part('parts/template/mainvisual/page');
+get_template_part('parts/breadcrumb');
 ?>
 
-
-<div class="p-content">
-<div class="l-inner">
-<div class="p-content__row">
-
-
-<main class="l-primary">
-<div class="p-content__page">
-
-<article <?php post_class( array( 'p-entry', 'p-entry--page' ) ); ?>>
-
-	<p>コンテンツが存在しません。</p>
-
-</article><!-- /.p-entry -->
-
-</div><!-- /.p-content__page -->
-</main><!-- /.l-primary -->
-
-
-<?php get_sidebar(); ?>
-
-
-</div><!-- /.p-content__row -->
-</div><!-- /.l-inner -->
-</div><!-- /.p-content -->
+<main>
+  <div class="inner">
+    <p>コンテンツが存在していません。</p>
+  </div>
+</main>
 
 
 <?php get_footer(); ?>
