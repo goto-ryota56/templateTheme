2a7b073fa2e8d6eda34fbf5520aee397e1e33c53
joetsu-goto <gotou0600@gmail.com>
2023-01-13 14:25:35 +0900

    不要要素削除

--- parts/pagenation-archive.php
+++ parts/pagenation-archive.php
@@ -1,24 +1,17 @@
-<?php
-/**
- * Pagenation
- */
-
- ?>
-
-<?php if ( paginate_links() ) : ?>
-<div class="p-pagenation">
-		<?php
-		echo wp_kses_post(
-			paginate_links(
-				array(
-					'end_size'  => 0,
-					'mid_size'  => 1,
-					'prev_next' => true,
-					'prev_text' => '<i class="fas fa-angle-left"></i>',
-					'next_text' => '<i class="fas fa-angle-right"></i>',
-				)
-			)
-		);
-		?>
-</div><!-- /.p-pagenation -->
+<?php if (paginate_links()) : ?>
+  <div class="pagenation_area">
+    <?php
+    echo wp_kses_post(
+      paginate_links(
+        array(
+          'end_size'  => 0,
+          'mid_size'  => 1,
+          'prev_next' => true,
+          'prev_text' => '<i class="fas fa-angle-left"></i>',
+          'next_text' => '<i class="fas fa-angle-right"></i>',
+        )
+      )
+    );
+    ?>
+  </div><!-- /.p-pagenation -->
 <?php endif; ?>
